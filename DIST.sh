d6f0589f0cd3b4e93fc688bc47c643a536e34b29
David Janes <davidjanes@davidjanes.com>
2017-08-03 05:04:19 -0400

    initial checkin

---
+++ DIST.sh
@@ -0,0 +1,45 @@
+#
+#   DIST.sh
+#
+#   David Janes
+#   IOTDB
+#   2017-08-03
+#
+
+exit 0
+PACKAGE=iotdb-fs
+DIST_ROOT=/var/tmp/.dist.$$
+
+if [ ! -d "$DIST_ROOT" ]
+then
+    mkdir "$DIST_ROOT"
+fi
+
+echo "=================="
+echo "NPM Packge: $PACKAGE"
+echo "=================="
+(
+    NPM_DST="$DIST_ROOT/$PACKAGE"
+    echo "NPM_DST=$NPM_DST"
+
+    if [ -d ${NPM_DST} ]
+    then
+        rm -rf "${NPM_DST}"
+    fi
+    mkdir "${NPM_DST}" || exit 1
+
+    update-package --increment-version --package "$PACKAGE" || exit 1
+
+    tar cf - \
+        --exclude "node_modules" \
+        README.md LICENSE \
+        package.json \
+        index.js \
+        lib/*.js \
+        |
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
+
+    echo "end"
+)
