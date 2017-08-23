cbf8b822883cb475f236eb70edd9d707b4f9bd72
David Janes <davidjanes@davidjanes.com>
2017-08-23 20:24:52 +0000

    test read functions

--- test/read.js
+++ test/read.js
@@ -34,8 +34,42 @@ describe("read", function() {
         describe("bad", function() {
         })
         describe("good", function() {
-            it("works", function(done) {
-                done();
+            it("using document_encoding latin1", function(done) {
+                Q({
+                    path: "data/c.txt",
+                    document_encoding: "latin1",
+                })
+                    .then(fs.read)
+                    .then(sd => {
+                        const expected_document = "Hello World\n你好，世界\nこんにちは世界\n";
+                        const expected_document_media_type = "text/plain";
+
+                        assert.ok(sd.document !== expected_document);
+                        assert.ok(sd.document.startsWith("Hello World\n"));
+                        assert.ok(!sd.document_encoding);
+                        assert.deepEqual(sd.document_media_type, expected_document_media_type);
+
+                        done();
+                    })
+                    .catch(done)
+            })
+            it("using document_encoding utf-8", function(done) {
+                Q({
+                    path: "data/c.txt",
+                    document_encoding: "utf-8",
+                })
+                    .then(fs.read)
+                    .then(sd => {
+                        const expected_document = "Hello World\n你好，世界\nこんにちは世界\n";
+                        const expected_document_media_type = "text/plain";
+
+                        assert.deepEqual(sd.document, expected_document);
+                        assert.deepEqual(sd.document_media_type, expected_document_media_type);
+                        assert.ok(!sd.document_encoding);
+
+                        done();
+                    })
+                    .catch(done)
             })
         })
     })
@@ -44,7 +78,22 @@ describe("read", function() {
         })
         describe("good", function() {
             it("works", function(done) {
-                done();
+                Q({
+                    path: "data/c.txt",
+                })
+                    .then(fs.read.buffer)
+                    .then(sd => {
+                        const expected_document = "Hello World\n你好，世界\nこんにちは世界\n";
+                        const expected_document_media_type = "text/plain";
+
+                        assert.ok(_.is.Buffer(sd.document))
+                        assert.deepEqual(sd.document.toString("utf-8"), expected_document);
+                        assert.deepEqual(sd.document_media_type, expected_document_media_type);
+                        assert.ok(!sd.document_encoding);
+
+                        done();
+                    })
+                    .catch(done)
             })
         })
     })
@@ -63,6 +112,7 @@ describe("read", function() {
 
                         assert.deepEqual(sd.document, expected_document);
                         assert.deepEqual(sd.document_media_type, expected_document_media_type);
+                        assert.ok(!sd.document_encoding);
 
                         done();
                     })
