e6a81bbe8d6e07dc8771a2e2c97f2dd1824b4bcd
David Janes <davidjanes@davidjanes.com>
2017-08-23 19:08:10 +0000

    recursive listing

--- test/list.js
+++ test/list.js
@@ -88,7 +88,52 @@ describe("list", function() {
         })
         describe("good", function() {
             it("works", function(done) {
-                done();
+                Q({
+                    path: "data",
+                })
+                    .then(fs.list.recursive)
+                    .then(sd => {
+                        const got = sd.paths.sort()
+                        const expected = [ 'data/a.json', 'data/b.json', 'data/c.txt', 'data/subfolder', "data/subfolder/a.json"  ];
+
+                        assert.deepEqual(got, expected)
+
+                        done();
+                    })
+                    .catch(done)
+            })
+            it("filter", function(done) {
+                Q({
+                    path: "data",
+                    filter: name => name.endsWith(".json"),
+                })
+                    .then(fs.list.recursive)
+                    .then(sd => {
+                        const got = sd.paths.sort()
+                        const expected = [ 'data/a.json', 'data/b.json', "data/subfolder/a.json" ];
+
+
+                        assert.deepEqual(got, expected)
+
+                        done();
+                    })
+                    .catch(done)
+            })
+            it("filter_path", function(done) {
+                Q({
+                    path: "data",
+                    filter_path: path => path.endsWith(".json") && path.startsWith("data/"),
+                })
+                    .then(fs.list.recursive)
+                    .then(sd => {
+                        const got = sd.paths.sort()
+                        const expected = [ 'data/a.json', 'data/b.json', "data/subfolder/a.json"  ];
+
+                        assert.deepEqual(got, expected)
+
+                        done();
+                    })
+                    .catch(done)
             })
         })
     })
